aa9a8872bfe16bf1f7ae773d8fe5285017d68e9c
Rene Gonzalez <66782975+PlayErphil@users.noreply.github.com>
2021-11-30 06:32:52 +0000

    Implemented list and single coin info functions. /help and /start command added.

--- api_calls.py
+++ api_calls.py
@@ -1,32 +1,33 @@
 import os
 import requests
-
-from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 
-WEB_API_KEY = os.environ['CRYPTO_WEB_API_KEY']
-
+WEB_API_KEY = os.environ['LCW_API_KEY']
 
 headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': WEB_API_KEY,
+    'content-type': 'application/json',
+    'x-api-key': WEB_API_KEY,
 }
-
 session = requests.Session()
 session.headers.update(headers)
 
 
-parameters = {
-  'start':'1',
-  'limit':'1',
-  'convert':'USD'
-}
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+def list(limit):
+    parameters = {
+        "currency": "USD",
+        "sort": "rank",
+        "order": "ascending",
+        "offset": 0,
+        "limit": limit,
+        "meta": False
+    }
+    url = 'https://api.livecoinwatch.com/coins/list'
+    r = session.post(url, data=json.dumps(parameters))
+    return r.json()
 
-try:
-  response = session.get(url, params=parameters)  
-  data = json.loads(response.text)
-  print(response.json())
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-  print(e)
-  
+
+def single(coin):
+    parameters = {"currency": "USD", "code": coin, "meta": True}
+    url = 'https://api.livecoinwatch.com/coins/single'
+    r = session.post(url, data=json.dumps(parameters))
+    return r.json()
--- main.py
+++ main.py
@@ -1,3 +1,96 @@
 import os
 import telebot
+import api_calls
+from millify import millify
 
+API_KEY = os.environ['API_KEY']
+bot = telebot.TeleBot(API_KEY)
+
+trash_info = {"color", "png32", "png64", "webp32", "webp64", "totalSupply", "pairs", "markets", "exchanges"}
+
+
+@bot.message_handler(commands=['help'])
+def info(message):
+    f = open('help.txt', 'r')
+    bot.send_message(message.chat.id, f.read())
+    f.close()
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(
+        message.chat.id,
+        "Hello! Welcome to Crypto Info Bot, use /help to learn how to use the bot. Enjoy!"
+    )
+
+
+def coin_request(message):
+    request = message.text.split()
+    if len(request) < 2 or request[0].lower() not in "info":
+        return False
+    else:
+        return True
+
+
+@bot.message_handler(func=coin_request)
+def send_coin_info(message):
+    coin = message.text.split()[1].upper()
+    data = api_calls.single(coin)
+    if 'error' in data:
+        bot.send_message(message.chat.id, "No data!?")
+    else:
+        response = ""
+
+        for val in data:
+            if val in "rate":
+                response += ("Price: " + format_value(data[val]) + "\n")
+            elif val not in trash_info:
+                response += (val.capitalize() + ": " +
+                             format_value(data[val]) + "\n")
+
+        bot.send_message(message.chat.id, response)
+
+
+def list_request(message):
+    request = message.text.split()
+    if len(request) < 2 or request[0].lower() not in "list":
+        return False
+    else:
+        return True
+
+
+@bot.message_handler(func=list_request)
+def send_list_info(message):
+    limit = int(message.text.split()[1])
+    data = api_calls.list(min(limit, 20))
+    if 'error' in data:
+        bot.send_message(message.chat.id, "No data!?")
+    else:
+        response = ""
+        for coin in data:
+            for val in coin:
+                if val in 'code':
+                    response += ("Name: ")
+                if val in 'rate':
+                    response += ("Price: ")
+                if val in 'volume':
+                    response += ("24H Volume: ")
+                if val in 'cap':
+                    response += ("Market Cap: ")
+                response += (format_value(coin[val]) + "\n")
+            response += "\n"
+
+        bot.send_message(message.chat.id, response)
+
+
+def format_value(val):
+    if type(val) == int or type(val) == float:
+        if val < 1:
+            return "$" + str(format(val, '.6f'))
+        else:
+            return "$" + str(millify(val, precision=2))
+    else:
+        return str(val)
+
+
+bot.polling()
