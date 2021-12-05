b6f2aac645ad1175b8b6b7e6906dc1cff23111f5
Rene Gonzalez <66782975+PlayErphil@users.noreply.github.com>
2021-12-05 16:44:59 +0000

    Calculator Bug fixed

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 import os
 import telebot
 import api_calls
+import json
 from millify import millify
 
 API_KEY = os.environ['API_KEY']
 bot = telebot.TeleBot(API_KEY)
 
 trash_info = {
-    "color",
-    "png32",
-    "png64",
-    "webp32",
-    "webp64",
-    "totalSupply",
-    "png128",
-    "webp128",
-    "uscompliant"
+    "color", "png32", "png64", "webp32", "webp64", "totalSupply", "png128",
+    "webp128", "uscompliant"
 }
 not_money = {
-    "pairs", "markets", "exchanges", "name", "symbol", "code", "visitors", "centralized"
+    "pairs", "markets", "exchanges", "name", "symbol", "code", "visitors",
+    "centralized"
 }
 
 
@@ -29,6 +24,17 @@ def info(message):
     bot.send_message(message.chat.id, f.read())
     f.close()
 
+    # Database
+    f = open("database.json", "r")
+    data = json.load(f)
+    f.close()
+    
+    data["help"]+=1
+    f = open("database.json", "w")
+    json.dump(data, f)
+    f.close()
+    
+
 
 # Start message
 @bot.message_handler(commands=['start'])
@@ -37,6 +43,15 @@ def start(message):
         message.chat.id,
         "Hello! Welcome to Crypto Info Bot, use /help to learn how to use the bot. Enjoy!"
     )
+    # Database
+    f = open("database.json", "r")
+    data = json.load(f)
+    f.close()
+    
+    data["users"]+=1
+    f = open("database.json", "w")
+    json.dump(data, f)
+    f.close()
 
 
 # Market overview request
@@ -77,9 +92,10 @@ def send_overview(message):
 
 # Single coin detailed information request
 def coin_request(message):
-    
+
     request = message.text.split()
-    if len(request) >= 2 and (request[0].lower() == "price" or request[0].lower() == "info"):
+    if len(request) >= 2 and (request[0].lower() == "price"
+                              or request[0].lower() == "info"):
         return True
     else:
         return False
@@ -88,10 +104,10 @@ def coin_request(message):
 @bot.message_handler(func=coin_request)
 def send_coin_info(message):
     coin = message.text.split()[1].upper()
-    if message.text.split()[0] in "info":
-      data = api_calls.single(coin, True)
+    if message.text.split()[0].lower() in "info":
+        data = api_calls.single(coin, True)
     else:
-      data = api_calls.single(coin, False)
+        data = api_calls.single(coin, False)
 
     if 'error' in data:
         print(data)
@@ -157,11 +173,13 @@ def send_list_info(message):
 
 
 # Exchanges list information request
-def list_ex_request(message):  
+def list_ex_request(message):
     request = message.text.split()
-    if len(request) < 3 or request[0].lower() != "list" or (request[1].lower() != "exchanges" and request[1].lower() != "exchange"):
+    if len(request) < 3 or request[0].lower() != "list" or (
+            request[1].lower() != "exchanges"
+            and request[1].lower() != "exchange"):
         return False
-        
+
     else:
         limit = request[2]
         if limit.isnumeric():
@@ -218,7 +236,7 @@ def send_ex_info(message):
         print(data)
         bot.send_message(message.chat.id, "No data!?")
     else:
-      
+
         response = ""
         for val in data:
             name = val.capitalize()
@@ -229,7 +247,7 @@ def send_ex_info(message):
                 name = "24H Volume"
             if val in "visitors":
                 name = "Daily Visitors"
-          
+
             if val.lower() in not_money:
                 money = False
 
@@ -237,6 +255,38 @@ def send_ex_info(message):
                 response += format_line(name, data[val], money)
 
         bot.send_message(message.chat.id, response)
+
+
+# Get real time price depending on amount
+def calculator_request(message):
+    request = message.text.split()
+    if len(request) < 3 or request[0].lower() != "calculate":
+        return False
+    else:
+        amount = request[2]
+        try:
+            float(amount)
+            return True
+        except ValueError:
+            return False
+
+
+@bot.message_handler(func=calculator_request)
+def send_value_calculated(message):
+    coin = message.text.split()[1].upper()
+    amount = float(message.text.split()[2])
+    data = api_calls.single(coin, False)
+    if "error" in data or data['rate'] == None:
+        print(data)
+        bot.send_message(message.chat.id, "No data!?")
+    else:
+
+        r = ""
+        r += format_line("Coin", coin.upper(), False)
+        r += format_line("Amount", amount, False)
+        r += format_line("Price", data['rate'], True)
+        r += format_line("Value", data['rate'] * amount, True)
+        bot.send_message(message.chat.id, r)
 
 
 # Format line function
